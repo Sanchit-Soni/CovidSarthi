0d5a90685554a7eb29de48ba3f12b21a546ce03f
animessa <Animesh.Saxena@amdocs.com>
2021-05-14 17:29:53 +0530

    changes for hospital card

--- src/Components/ListCard.js
+++ src/Components/ListCard.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Card, Col, NavLink, Row } from "react-bootstrap";
-
+import { Button, Card, Col, NavLink, Row,ListGroup } from "react-bootstrap";
+import '../App.css';
 const ListCard = ({
   id,
   city,
@@ -16,64 +16,77 @@ const ListCard = ({
   const contactSplit = contact.split(",");
 
   return (
-    <Card className="my-3 p-3 card_design">
+    <Card class="card-deco">
+      <Card.Header><i class="fas fa-hospital"></i> Hospitals in {city}</Card.Header>
       <Card.Body>
-        <Card.Title as="div">
+        {/*<Card.Title as="div">
           <center>
             <strong>
               <h3>{city}</h3>
             </strong>
           </center>
-          <hr></hr>
-        </Card.Title>
-        <Row>
-          <h5 className="hos-head">
-            <span>
-              <i class="fas fa-hospital"></i>
-            </span>{" "}
-            : {hospital}
-          </h5>
-        </Row>
-        <Row className="row-data">
-          <Col>
-            <Card.Text>Total Beds: {totalBeds}</Card.Text>
-            <Card.Text>Available Beds: {avail_beds}</Card.Text>
+  </Card.Title>*/}
+        <Card.Text>
+          <h5 class="text-center">
+            {hospital}
+          </h5><hr></hr>
+          <ListGroup>
+            <ListGroup.Item>
+            <Row>
+            <Col xs={7}>Status</Col>
+            <Col xs={5}>{" "}{bed_status ? (<i class="far fa-check-circle"></i>
+              ) : (
+                <i class="fas fa-times"></i>
+              )}</Col>
+          </Row>
+            </ListGroup.Item>
 
-            <Card.Text>
+          {/*<Card.Text>
               Status:{" "}
               {bed_status ? (
                 <i class="far fa-check-circle"></i>
               ) : (
                 <i class="fas fa-times"></i>
               )}
-            </Card.Text>
-          </Col>
-
+              </Card.Text>*/}
+            <ListGroup.Item>
+            <Row>
+            <Col xs={7}>Total Beds</Col>
+            <Col xs={5}>{totalBeds}</Col>
+            </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+          <Row>
+            <Col xs={7}>Available Beds</Col>
+            <Col xs={5}>{avail_beds}</Col>
+          </Row></ListGroup.Item>
+            </ListGroup>
+            {/*<Card.Text>Total Beds: {totalBeds}</Card.Text><Card.Text>Available Beds: {avail_beds}</Card.Text>*/}
+          {/*
           <Col>
             <Card.Text>
               {bedDivision.map((data, i) => (
                 <p>{data}</p>
               ))}
             </Card.Text>
-          </Col>
-        </Row>
-        <a href={address}>
-          {" "}
-          Location <i class="fas fa-map-marker-alt" size="3px"></i>
-        </a>
-        <hr></hr>
-        <Card.Text>
-          <h5>
-            Contacts{" "}
-            <span>
-              <i class="fas fa-phone-square"></i>
-            </span>
-          </h5>
+              </Col>*/} 
+          <br></br>
+          <div class="border text-justify">
+          <h6 class="contacts">
+          <i class="fas fa-phone-square-alt"></i> Contacts{" "}
+          </h6>
+          <ul>
           {contactSplit.map((data, i) => (
-            <p>{data}</p>
-          ))}
+            <p><i class="fas fa-hospital-user icon-space"></i>{data}</p>
+          ))}</ul></div>
         </Card.Text>
       </Card.Body>
+      <Card.Footer className="text-center">
+             <small>
+             <i class="fas fa-map-marker-alt" size="3px"></i>
+              <a href={address}>{" "} Location</a>
+             </small>
+        </Card.Footer>
     </Card>
   );
 };
--- src/Firebase.js
+++ src/Firebase.js
@@ -3,14 +3,14 @@ import database from "firebase/database";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-  apiKey: "AIzaSyDVmP2DyMY0eXgK-JRZRH-iLZ5wjd29ll8",
-  authDomain: "sheet-2-firebase-ef219.firebaseapp.com",
-  databaseURL: "https://sheet-2-firebase-ef219-default-rtdb.firebaseio.com",
-  projectId: "sheet-2-firebase-ef219",
-  storageBucket: "sheet-2-firebase-ef219.appspot.com",
-  messagingSenderId: "543724438312",
-  appId: "1:543724438312:web:e76a7be4c737a2dca0461d",
-  measurementId: "G-507EJSP371",
+  apiKey: "AIzaSyDgb-Vquxg3EMrlfRCAZ2_sb3P5Y1lhI3E",
+  authDomain: "covidsarthi.firebaseapp.com",
+  databaseURL: "https://covidsarthi-default-rtdb.firebaseio.com",
+  projectId: "covidsarthi",
+  storageBucket: "covidsarthi.appspot.com",
+  messagingSenderId: "819530536777",
+  appId: "1:819530536777:web:9d698c162b24a2e260ec98",
+  measurementId: "G-VWG8XKN0FV"
 };
 
 firebase.initializeApp(config);
